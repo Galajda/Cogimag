fd5831c2e14d688b7a69fb4cf91ab29363abcdc5
Galajda <33378775+Galajda@users.noreply.github.com>
2018-02-03 13:57:26 -0600

    probably not going to use this

---
+++ src/cogimag/java/keyboard/KeyNamesEnum.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2018 Michal G.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cogimag.java.keyboard;
+
+/**
+ *
+ * @author Michal G. <Michal.G at cogitatummagnumtelae.com>
+ */
+public enum KeyNamesEnum {
+    EXCLAMATION, DOUBLE_QUOTE, POUND, DOLLAR, PERCENT, AMPERSAND, SINGLE_QUOTE,
+    LEFT_PAREN, RIGHT_PAREN, ASTERISK, PLUS, COMMA, MINUS, PERIOD, FWD_SLASH,
+    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
+    COLON, SEMI_COLON, LESS_THAN, QUESTION, AT,
+    UCASE_A, UCASE_B, UCASE_Z
+    
+    
+    
+    
+    
+    
+}
