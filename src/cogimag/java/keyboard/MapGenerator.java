0ccc32e8ae786c71d69dc44eed82bd89a3e8094c
Michal G. <33378775+Galajda@users.noreply.github.com>
2018-02-16 22:27:00 -0600

    Added documentation. Enabled Copy to clipboard button. Housekeeping.

--- src/cogimag/java/keyboard/MapGenerator.java
+++ src/cogimag/java/keyboard/MapGenerator.java
@@ -19,16 +19,16 @@ package cogimag.java.keyboard;
 import java.awt.event.KeyEvent;
 import java.lang.reflect.Field;
 //import java.lang.reflect.Modifier;
-import java.awt.Robot;
-import java.awt.AWTException;
+//import java.awt.Robot;
+//import java.awt.AWTException;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+//import java.util.ArrayList;
+//import java.util.HashMap;
 import javafx.scene.input.KeyCode;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -40,7 +40,7 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 /**
- * Main method to be run during application development. <br/>
+ * Main method to be run during application development. <br>
  * This class automates the process of creating a HashMap for use by Robot or manually triggered KeyEvent.
  * The class provides a simple window with a text input box and a text output box. The user will 
  * type one character from the keyboard into the text input  box. The keyPressed event captures the 
@@ -53,27 +53,24 @@ import javax.swing.UnsupportedLookAndFeelException;
  * the characters on the client's keyboard. The built-in KeyMap class contains such a list for the US-English
  * QWERTY keyboard. If the client uses a different keyboard, the developer may extend the built-in class
  * and hide the MakeMap() method with a new method containing the client's keyboard map.
- * 
- * 
- * " and \ need escape char. currently this must be fixed manually
- * <br/><br/>
- * adapted from KeyEventDemo.java (attached) by Oracle.
- * Help at https://docs.oracle.com/javase/tutorial/uiswing/events/keylistener.html
- 
+ * <br><br>
+ * Known issue: " and \ need escape char. Currently this must be fixed manually after the put statements are generated.
+ * <br><br>
+ * Adapted from KeyEventDemo.java (attached) by Oracle. Read the full article  at 
+ * <a href="https://docs.oracle.com/javase/tutorial/uiswing/events/keylistener.html">Oracle key listener tutorial</a>
  * @author Michal G. <Michal.G at cogitatummagnumtelae.com>
- 
  */
 public class MapGenerator extends JFrame implements KeyListener, ActionListener {
-    
+    //alternative: make components final. declare them in constructor. assign listener in the runnable
     private JTextField txtInput;
     private JScrollPane paneOutputContainer;
     private JTextArea txtOutput;    
     private JSplitPane paneButtonContainer;
     private JButton btnClear;
-    private JButton btnCopy;
-    
-    static final String newline = System.getProperty("line.separator");
-    
+    private static final String BTN_CLEAR_TEXT = "Clear contents";
+    private JButton btnCopy;    
+    private static final String BTN_COPY_TEXT = "Copy to clipboard";
+//    private static final String NEWLINE = System.getProperty("line.separator");    
     private String displayedChar;
     private int vkNumber;
     private boolean isShifted;
@@ -148,9 +145,9 @@ public class MapGenerator extends JFrame implements KeyListener, ActionListener 
 //        paneOutputContainer.revalidate();
         getContentPane().add(paneOutputContainer, BorderLayout.CENTER);
         
-        btnClear = new JButton("Clear contents");
+        btnClear = new JButton(MapGenerator.BTN_CLEAR_TEXT);
         btnClear.addActionListener(this);       
-        this.btnCopy = new JButton("Copy to clipboard");
+        this.btnCopy = new JButton(MapGenerator.BTN_COPY_TEXT);
         btnCopy.addActionListener(this);
         paneButtonContainer = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,btnClear,btnCopy);
         getContentPane().add(paneButtonContainer, BorderLayout.PAGE_END);
@@ -163,8 +160,8 @@ public class MapGenerator extends JFrame implements KeyListener, ActionListener 
             //get key code and modifier
 //            txtOutput.append(KeyParser.getDisplayInfo(e, "Key pressed: "));
 //            txtOutput.append("key pressed event");
-//            txtOutput.append("\tkey code = " + e.getKeyCode() + newline); //VK_ number
-//            txtOutput.append("\t\tmodifier = " + KeyEvent.getModifiersExText(e.getModifiersEx()) + newline);
+//            txtOutput.append("\tkey code = " + e.getKeyCode() + NEWLINE); //VK_ number
+//            txtOutput.append("\t\tmodifier = " + KeyEvent.getModifiersExText(e.getModifiersEx()) + NEWLINE);
 //            txtOutput.setCaretPosition(txtOutput.getDocument().getLength());
             vkNumber = e.getKeyCode();
             isShifted = ("Shift".equals(KeyEvent.getModifiersExText(e.getModifiersEx())));
@@ -179,8 +176,8 @@ public class MapGenerator extends JFrame implements KeyListener, ActionListener 
         //get key char
 //        txtOutput.append("key typed event");
         int asciiNumber = Character.toString(e.getKeyChar()).codePointAt(0);
-//        txtOutput.append("\t\tkey char = " + e.getKeyChar() + newline); //char as printed
-//        txtOutput.append("\t\tascii # " + asciiNumber + newline);
+//        txtOutput.append("\t\tkey char = " + e.getKeyChar() + NEWLINE); //char as printed
+//        txtOutput.append("\t\tascii # " + asciiNumber + NEWLINE);
 //        txtOutput.setCaretPosition(txtOutput.getDocument().getLength());
         displayedChar = String.valueOf(e.getKeyChar());
         charCon = new CharConstruction(displayedChar, vkNumber, isShifted);
@@ -205,9 +202,22 @@ public class MapGenerator extends JFrame implements KeyListener, ActionListener 
     public void actionPerformed(ActionEvent e) {
         //split clear and copy
 //        e.getID()
-        System.out.println("btnClear click");
-        txtInput.setText("");
-        txtOutput.setText("");
+        JButton btn = (JButton) e.getSource();
+        
+        System.out.println("button text " + btn.getText());
+        switch (btn.getText()) {
+            case MapGenerator.BTN_CLEAR_TEXT:                
+//                System.out.println("btnClear click");
+                txtInput.setText("");
+                txtOutput.setText("");
+                break;
+            case MapGenerator.BTN_COPY_TEXT:
+                txtOutput.selectAll();
+                txtOutput.copy();
+                break;
+            default:
+                
+        }
         txtInput.requestFocusInWindow();
     }
     public static String formatMapPut(int ascii_number, CharConstruction char_con) {
@@ -231,8 +241,7 @@ public class MapGenerator extends JFrame implements KeyListener, ActionListener 
 //        System.out.println("iterating through key event static fields");
         String returnVal = null;
         Field[] classFields = KeyEvent.class.getDeclaredFields();
-        for (Field f : classFields) {
-            
+        for (Field f : classFields) {            
             if (f.getName().startsWith("VK_")) {
                 int vkValue;
                 try {
@@ -248,8 +257,7 @@ public class MapGenerator extends JFrame implements KeyListener, ActionListener 
 //                System.out.print("\tfound " + f.getName() + " = " + vkValue + " key text :" 
 //                        + keyText + ":" + " matches test value " + testVal);
 //                System.out.println("char from code point :" + Character.toString((char)s.codePointAt(0)));
-                    //same as s
-                
+                    //same as s                
             }
         }
         return returnVal;
