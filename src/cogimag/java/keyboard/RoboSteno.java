7be9ee9f241a584b08af843cba9e94400400a930
Michal G. <33378775+Galajda@users.noreply.github.com>
2018-02-17 15:25:59 -0600

    Robot typist works with the KeyMap to print characters on the screen

---
+++ src/cogimag/java/keyboard/RoboSteno.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cogimag.java.keyboard;
+
+//import com.sun.jna.Native;
+import java.awt.Robot;
+import java.awt.AWTException;
+//import java.lang.SecurityException;
+import java.awt.event.KeyEvent;
+//import static java.lang.System.out;
+//import javafx.scene.input.KeyCode;
+
+/**
+ *
+ * @author MichalG HP Envy
+ */
+public class RoboSteno {
+    
+    private static Robot r;
+    private static boolean isRobotOnline;    
+    
+    
+    public RoboSteno() {
+        isRobotOnline = true;
+        try {
+            r = new Robot();
+        }        
+        catch (AWTException | SecurityException ex)
+        {
+            System.out.println("cannot initialize robot" + ex.getMessage());
+            isRobotOnline = false;
+        }
+    }
+    /**
+     * Fundamental typing method. Overloaded methods call this to print characters on the screen.
+     * Method checks for availability of the Robot. If offline, no chars are typed, no error
+     * is thrown.
+     * @param charCon 
+     */
+    public void type(CharConstruction charCon) {
+        if (isRobotOnline) {            
+            if (charCon.isShifted) { r.keyPress(KeyEvent.VK_SHIFT); }
+            r.keyPress(charCon.keyEventConstant);
+            r.keyRelease(charCon.keyEventConstant);
+            if (charCon.isShifted) { r.keyRelease(KeyEvent.VK_SHIFT); }            
+            r.delay(10);
+        }        
+    }
+    
+    public void type(char c) {
+        type(getCharCon(c));
+//        return true;
+    }
+//    public static void type(int ascii) {
+////        return true;
+//    }
+    public void type(String s) {
+        char[] charArray = s.toCharArray();
+        for (char c : charArray) {
+            type(getCharCon(c));
+        }
+//        return true;
+    }
+    
+    private static CharConstruction getCharCon(char c) {
+        return KeyMap.CHAR_MAP.get(Character.toString(c).codePointAt(0));
+    }
+    
+    private static CharConstruction getCharCon(int ascii_code) {
+        return KeyMap.CHAR_MAP.get(ascii_code);
+    }
+//    private static int getVkCode(char c) {
+////        KeyMap.CHAR_MAP.get(c);
+//        CharConstruction charCon = KeyMap.CHAR_MAP.get(Character.toString(c).codePointAt(0));
+//        return charCon.keyEventConstant;
+//    }
+    
+//    private static int getVkCode(int ascii_code) {
+//        CharConstruction charCon = KeyMap.CHAR_MAP.get(ascii_code);
+//        return charCon.keyEventConstant;
+//    }
+    
+//    public void type(int vk_code) {
+//        r.keyPress(vk_code);
+//        r.keyRelease(vk_code);
+//        r.delay(10);
+////        return true;
+//    }
+}
